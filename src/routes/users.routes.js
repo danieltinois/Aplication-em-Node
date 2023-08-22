329d0b1e0fd8af9d1e920bbfb5c91be5e7cd6cbb
Daniel <99421761+danieltinois@users.noreply.github.com>
2023-08-21 23:28:42 -0300

    Add files via upload

---
+++ src/routes/users.routes.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController");
+
+const usersRoutes = Router();
+
+const usersController = new UsersController();
+
+// function myMiddleware(request, response, next) {
+//   console.log("Você passou pelo Middleware!");
+
+//   if (!request.body.isAdmin) {
+//     return response.json({ message: "user unauthorized" });
+//   }
+
+//   next();
+// }
+
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
+
+module.exports = usersRoutes;
